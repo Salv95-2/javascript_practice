f00fbac4554c364b781cbcba950971fc9d68a4ec
Salv95-2 <salvramirez95@gmail.com>
2019-05-04 22:05:23 -0700

    Adding more javascript practice examples commented out

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,89 @@
 //console.log('Hello World!!!!');
 
-const cardId = 42;
 
+// let app = (function(){
+//     let carId  = 123;
+//     let getId = function(){
+//         return carId;
+//     };
 
+//     return{
+//         getIdP: getId
+//     };
+    
+// })();
 
+// console.log(app.getIdP());
 
-let x = 5;
+// //call
+// let o = {
+//     cardId: 123,
+//     getId: function(){
+//         return this.cardId;
+//     }
+// };
 
-console.log(++x);  
-console.log(++x);
+// let newCar = {cardId: 456};
 
-console.log(x);
+// console.log( o.getId.call(newCar) );//456
 
+//apply
+// let o = {
+//     cardId: 123,
+//     getId: function(prefix, prefix2){
+//         return prefix + this.cardId + prefix2;
+//     }
+// };
+
+// let newCar = {cardId: 456};
+
+// console.log( o.getId.apply(newCar, ['ID: ', 'd']) );//456
+
+// //bind
+// let o = {
+//     cardId: 123,
+//     getId: function(){
+//         return this.cardId;
+//     }
+// };
+
+// let newCar = {cardId: 456};
+
+// let newFn =  o.getId.bind(newCar);
+// console.log(newFn());//456
+
+// // Arror Functions no paramenter
+
+// let getId = () => 123;
+
+// console.log(getId());
+
+// //Arrow Function with parameters 
+
+// let getId = (prefix, prefix2) => prefix + prefix2;
+
+// console.log(getId(4,2));
+
+// //Arrow Function with return statement 
+
+// let getId = (prefix, prefix2) => {
+    
+//     return prefix + prefix2;
+
+// };
+// console.log(getId(4,2));
+
+// //Arrow function with underscore
+
+// let getId = _ => 123;
+
+// console.log(getId());
+
+//Default parameters and interpolating
+let trackCar = function(cardId, city = 'NY'){
+    console.log(`Tracking ${cardId} in ${city}.`);
+};
+
+console.log( trackCar(123) );
+
+console.log( trackCar(123, 'Chicago'));
