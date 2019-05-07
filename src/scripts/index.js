953c84a5b85b3e0d9bbbf3bcad639a53b460fd2a
Salv95-2 <salvramirez95@gmail.com>
2019-05-07 00:23:34 -0700

    Adding example JS code from 'Objects and Arrays' Module

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -89,14 +89,132 @@
 // console.log( trackCar(123, 'Chicago'));
 
 
-//Constructor Function
+// //Constructor Function
 
-function Car(id){
-    this.carId = id;
-    this.start = function(){
-        console.log('start: ' + this.carId);
-    };
-}
+// function Car(id){
+//     this.carId = id;
+//     this.start = function(){
+//         console.log('start: ' + this.carId);
+//     };
+// }
 
-let car = new Car(123);
-car.start();  
+// let car = new Car(123);
+// car.start();  
+
+// Prototypes
+
+// function Car(id){
+//     this.cardId = id;
+// }
+
+// Car.prototype.start =  function(){
+//     console.log('start: ' +  this.cardId);
+// };
+
+// let car = new Car(123);
+// car.start();
+
+// //expanding protoypes
+
+// String.prototype.hello = function(){
+//     return this.toString() + ' Hello ';
+// };
+
+// // console.log('foo'.hello()); // Foo Hello
+
+// let car = {
+//     id:123,
+//     style: 'convertible'
+// };
+
+// console.log(JSON.stringify(car));
+
+
+
+// let carIds = [
+//     { carId: 123 },
+//     { carId: 456 },
+//     { carId: 789 }
+// ];
+
+// console.log( JSON.stringify(carIds));
+
+
+// // Parsing JSON
+
+
+// let jsonIn = `[
+//     {"carId" : 123},
+//     {"carId": 456},
+//     {"carId": 789}
+// ]`;
+
+// let carIds = JSON.parse(jsonIn);
+// console.log(carIds);
+
+
+
+//Array Interation
+
+// let carIds = [
+//     {cardId: 123, style: 'sedan'},
+//     {cardId: 456, style: 'convertible'},
+//     {cardId: 789, style: 'sedan'}
+    
+// ];
+
+// //carIds.forEach(car => console.log(car));
+
+// carIds.forEach((car,index) => console.log(car,index));
+
+
+// //Array Filtering
+
+
+// let carIds = [
+//     {cardId: 123, style: 'sedan'},
+//     {cardId: 456, style: 'convertible'},
+//     {cardId: 789, style: 'sedan'}
+    
+// ];
+
+// let convertibles = carIds.filter(
+//     car => car.style === 'convertible'
+// );
+
+// console.log(convertibles);
+
+// //Array Testing
+
+// let carIds = [
+//     {carId: 123, style: 'sedan'},
+//     {carId: 456, style: 'convertible'},
+//     {carId: 789, style: 'sedan'}
+    
+// ];
+
+// let result = carIds.every(
+//     car => car.carId > 0
+// );
+
+// console.log(result);
+
+//Locate the first Match
+
+let carIds = [
+    {carId: 123, style: 'sedan'},
+    {carId: 456, style: 'convertible'},
+    {carId: 789, style: 'sedan'}
+    
+];
+
+let car = carIds.find(
+    car => car.carId > 500
+);
+
+console.log(car);
+
+
+
+
+
